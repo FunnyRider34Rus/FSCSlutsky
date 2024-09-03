cc568368ffdea51b5ff1680046b54c749481814e
elpablo <elpablo.volgograd@yandex.ru>
2024-09-03 11:49:12 +0300

    set light colors by default

--- app/src/main/java/com/elpablo/fscslutsky/core/theme/Theme.kt
+++ app/src/main/java/com/elpablo/fscslutsky/core/theme/Theme.kt
@@ -1,16 +1,8 @@
 package com.elpablo.fscslutsky.core.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Blue40,
-    secondary = Blue80,
-    tertiary = Gray40
-)
 
 private val LightColorScheme = lightColorScheme(
     primary = Blue40,
@@ -20,16 +12,10 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun FSCSlutskyTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = LightColorScheme,
         typography = Typography,
         content = content
     )
